4e44d8c43d625abb87ea0836ee2cd94d4d90cfd1
jordanchicksen1 <2333640@students.wits.ac.za>
2026-02-12 00:35:12 +0200

    did some things

--- MICRO_PLATFORMER/Assets/scripts/obstacles/DestroyBox.cs
+++ MICRO_PLATFORMER/Assets/scripts/obstacles/DestroyBox.cs
@@ -4,7 +4,7 @@ using UnityEngine.VFX;
 public class DoorKillBox : MonoBehaviour
 {
     public VisualEffect smokePoof;
-
+    public float destroyTime = 0.125f;
     public void Start()
     {
         smokePoof.Stop();
@@ -17,7 +17,7 @@ public class DoorKillBox : MonoBehaviour
             smokePoof.Stop();      // Ensure it's reset
             smokePoof.Play();      // Play once
 
-            Destroy(other.gameObject, 0.1f);
+            Destroy(other.gameObject, destroyTime);
             Debug.Log("door entered");
         }
     }
