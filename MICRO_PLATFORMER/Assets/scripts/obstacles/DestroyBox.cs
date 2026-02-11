cf00820af8d457dc56fd0155cadd1c23ab555aa9
jordanchicksen1 <2333640@students.wits.ac.za>
2026-02-12 00:07:56 +0200

    visual effects are sort of working

--- MICRO_PLATFORMER/Assets/scripts/obstacles/DestroyBox.cs
+++ MICRO_PLATFORMER/Assets/scripts/obstacles/DestroyBox.cs
@@ -1,13 +1,23 @@
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class DoorKillBox : MonoBehaviour
 {
+    public VisualEffect smokePoof;
+
+    public void Start()
+    {
+        smokePoof.Stop();
+    }
+
     void OnTriggerEnter(Collider other)
     {
-        
         if (other.CompareTag("Door"))
         {
-            Destroy(other.gameObject);
+            smokePoof.Stop();      // Ensure it's reset
+            smokePoof.Play();      // Play once
+
+            Destroy(other.gameObject, 0.1f);
             Debug.Log("door entered");
         }
     }
