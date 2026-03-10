a2d81d21630e6d6c19f5ead7b631c47d1cd8f8f2
jordanchicksen1 <2333640@students.wits.ac.za>
2026-03-11 00:21:21 +0200

    added numbers to enemy gates for clarity

--- MICRO_PLATFORMER/Assets/scripts/obstacles/EnemyGroupGate.cs
+++ MICRO_PLATFORMER/Assets/scripts/obstacles/EnemyGroupGate.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections.Generic;
+using TMPro;
 
 public class EnemyGroupGate : MonoBehaviour
 {
@@ -8,13 +10,27 @@ public class EnemyGroupGate : MonoBehaviour
     [Header("Camera Focus")]
     [SerializeField] Transform focusPoint;
 
+    [Header("Kill Counter Popup")]
+    [SerializeField] GameObject counterPopupPrefab;
+
+    [Header("Door Counter")]
+    [SerializeField] TextMeshProUGUI doorCounterText;
+
     DoorCameraFocus cameraFocus;
 
+    HashSet<Enemy> countedEnemies = new HashSet<Enemy>();
+
+    int killCount;
     bool opened;
 
     void Awake()
     {
         cameraFocus = FindFirstObjectByType<DoorCameraFocus>();
+    }
+
+    void Start()
+    {
+        UpdateDoorCounter();
     }
 
     void Update()
@@ -23,15 +39,49 @@ public class EnemyGroupGate : MonoBehaviour
 
         foreach (Enemy e in enemies)
         {
-            if (e != null && !e.IsDead)
-                return;
+            if (e == null) continue;
+
+            if (e.IsDead && !countedEnemies.Contains(e))
+            {
+                countedEnemies.Add(e);
+                killCount++;
+
+                SpawnPopup(e.transform.position, killCount);
+                UpdateDoorCounter();
+            }
         }
 
-        opened = true;
+        if (killCount >= enemies.Length)
+        {
+            opened = true;
 
-        bool firstTime = door.Open();
+            bool firstTime = door.Open();
 
-        if (firstTime && cameraFocus && focusPoint)
-            cameraFocus.FocusOn(focusPoint);
+            if (doorCounterText)
+                doorCounterText.text = enemies.Length + " / " + enemies.Length;
+
+            if (firstTime && cameraFocus && focusPoint)
+                cameraFocus.FocusOn(focusPoint);
+        }
+    }
+
+    void SpawnPopup(Vector3 position, int number)
+    {
+        if (!counterPopupPrefab) return;
+
+        Vector3 spawnPos = position + Vector3.up * 1.5f;
+
+        GameObject popup = Instantiate(counterPopupPrefab, spawnPos, Quaternion.identity);
+
+        EnemyKillPopup popupScript = popup.GetComponent<EnemyKillPopup>();
+        if (popupScript)
+            popupScript.SetNumber(number);
+    }
+
+    void UpdateDoorCounter()
+    {
+        if (!doorCounterText) return;
+
+        doorCounterText.text = killCount + " / " + enemies.Length;
     }
 }
---
+++ MICRO_PLATFORMER/Assets/scripts/obstacles/EnemyKillPopup.cs
@@ -0,0 +1,61 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class EnemyKillPopup : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI text;
+
+    [Header("Motion")]
+    [SerializeField] float riseSpeed = 2f;
+
+    [Header("Animation")]
+    [SerializeField] float popScale = 1.6f;
+    [SerializeField] float popDuration = 0.15f;
+
+    [Header("Lifetime")]
+    [SerializeField] float lifeTime = 1f;
+
+    float timer;
+    Vector3 baseScale;
+    Color startColor;
+
+    void Awake()
+    {
+        baseScale = transform.localScale;
+
+        if (text)
+            startColor = text.color;
+    }
+
+    public void SetNumber(int number)
+    {
+        if (text)
+            text.text = number.ToString();
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        // FLOAT
+        transform.position += Vector3.up * riseSpeed * Time.deltaTime;
+
+        // POP SCALE
+        if (timer < popDuration)
+        {
+            float t = timer / popDuration;
+            float scale = Mathf.Lerp(popScale, 1f, t);
+            transform.localScale = baseScale * scale;
+        }
+
+        // COLOUR SHIFT DURING ENTIRE LIFETIME
+        if (text)
+        {
+            float t = timer / lifeTime;
+            text.color = Color.Lerp(startColor, Color.white, t);
+        }
+
+        if (timer >= lifeTime)
+            Destroy(gameObject);
+    }
+}
